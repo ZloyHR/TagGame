bfae63312c385429b4edddb824b6cd604b8ca08f
ZloyHR <dobrovolskyivan2002@gmail.com>
2021-01-02 18:06:16 +0200

    Box move with mouse OnDrag

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,8 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    
+    private Vector2 _prevCursor;
+
+    private void Awake()
+    {
+        _prevCursor = Vector2.negativeInfinity;
+    }
+
+
+    private void OnMouseDown()
+    {
+        _prevCursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 currentCursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 delta = currentCursor - _prevCursor;
+        transform.Translate(delta);
+
+        _prevCursor = currentCursor;
+    }
 }
